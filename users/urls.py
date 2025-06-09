577a73a08fa19c5b66a78aa254a670d842dd9532
JV <126692+vilosource@users.noreply.github.com>
2025-06-09 19:16:05 +0300

    Add demo profile TemplateView

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,6 @@ app_name = 'users'
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    path('profile/', views.user_profile, name='profile'),
+    path('profile/', views.UserProfileView.as_view(), name='profile'),
     path('change-password/', views.change_password, name='change_password'),
 ]
